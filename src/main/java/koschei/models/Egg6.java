e8ea43fcc8bfe133d61847e635a5d24673c2cdb5
Makariyp <makariyp@icloud.com>
2023-02-14 13:50:25 +0300

    Задание выполнено

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,16 @@
 package koschei.models;
 
-public class Egg6 {
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class Egg6 {
+    private Needle7 needle;
+
+    @Autowired
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }

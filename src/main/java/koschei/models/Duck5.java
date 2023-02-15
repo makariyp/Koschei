2b712178a99ca8ff8b51d10184832f5c21e8719e
Makariyp <makariyp@icloud.com>
2023-02-15 09:19:25 +0300

    Добавил связь через сеттер

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,12 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-    @Autowired
     private Egg6 egg;
 
     public Duck5(Egg6 egg) {
         this.egg = egg;
     }
+
+    @Autowired
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
+    }
+
     @Override
     public String toString() {
         return ", в утке яйцо " + egg.toString();
